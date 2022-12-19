69ed8ced64540ab3b5071fedfbfff4e8da4ecb76
FlyingPretzel <114416661+FlyingPretzel-Labs@users.noreply.github.com>
2022-12-19 21:15:51 +0100

    Update main.py
    Changed Telegram code as the previous was not working as intended.

--- main.py
+++ main.py
@@ -1,54 +1,29 @@
 import time
-import requests
-import json
 import psutil
+import telegram
 
-chat_id = "5839085506:AAGyxBqxfhHG_pG9kr2PCtKBE6J4SSXkBVc"
-api_key = "1560942074"
+#chat_id = "1560942074"
+#api_key = "5839085506:AAGyxBqxfhHG_pG9kr2PCtKBE6J4SSXkBVc"
 
-# Telegram API (not entirely sure what this all does but it defines send_telegram_message so that we can use it
-def send_telegram_message(message: str,
-                          chat_id: str,
-                          api_key: str,
-                          proxy_username: str = None,
-                          proxy_password: str = None,
-		  proxy_url: str = None):
-    responses = {}
-
-    proxies = None
-    if proxy_url is not None:
-        proxies = {
-            'https': f'http://{username}:{password}@{proxy_url}',
-            'http': f'http://{username}:{password}@{proxy_url}'
-        }
-        headers = {'Content-Type': 'application/json',
-                   'Proxy-Authorization': 'Basic base64'}
-        data_dict = {'chat_id': chat_id,
-                     'text': message,
-                     'parse_mode': 'HTML',
-                     'disable_notification': True}
-        data = json.dumps(data_dict)
-        url = f'https://api.telegram.org/bot{api_key}/sendMessage'
-        response = requests.post(url,
-                                 data=data,
-                                 headers=headers,
-                                 proxies=proxies,
-                                 verify=False)
-        return response
+bot = telegram.Bot(token="5839085506:AAGyxBqxfhHG_pG9kr2PCtKBE6J4SSXkBVc")
 
 while True:
     try:
         # Get the current CPU percentage
         cpu_percentage = psutil.cpu_percent()
 
-        # If the CPU percentage is over 5%, send a notification
-        if cpu_percentage > 5.0:
-            send_telegram_message("CPU percentage is over 5%! Current value: {}".format(cpu_percentage), chat_id, api_key)
+        # Print the CPU percentage
+        print('System CPU load is {} %'.format(cpu_percentage))
+
+        # If the CPU percentage is over 1%, send a notification
+        if cpu_percentage > 0.5:
+            bot.sendMessage(chat_id=1560942074, text="CPU percentage is over 1%! Current value: {}".format(cpu_percentage))
+            #the break can be removed if we want to continuously run the script
+            break
+
+        # Sleep for 5 second before checking again
+        time.sleep(5)
     except Exception as e:
         # Print any exceptions that occur
         print(e)
-
-    # Sleep for 5 seconds before checking again
-    time.sleep(5)
-
-send_telegram_message("Monitoring script is up and running is up and running", chat_id, api_key)
+	
