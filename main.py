4be38df7666906dce9914cc752a5c0eb92826c8d
FlyingPretzel <114416661+FlyingPretzel-Labs@users.noreply.github.com>
2022-12-19 17:48:25 +0100

    Update main.py
    Added print functions

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ bot = telegram.Bot(token='5839085506:AAGyxBqxfhHG_pG9kr2PCtKBE6J4SSXkBVc')
 chat_id = 1560942074
 
 while True:
-    # Get the current CPU percentage
-    cpu_percentage = psutil.cpu_percent()
+    try:
+        # Get the current CPU percentage
+        cpu_percentage = psutil.cpu_percent()
 
-    # If the CPU percentage is over 5%, send a notification
-    if cpu_percentage > 5.0:
-        bot.send_message(chat_id=chat_id, text='CPU percentage is over 5%! Current value: {}'.format(cpu_percentage))
-    
-    # Sleep for 10 Sec before checking again
-    time.sleep(10)
+        # Print the current CPU percentage
+        print("Current CPU percentage: {}".format(cpu_percentage))
+
+        # If the CPU percentage is over 5%, send a notification
+        if cpu_percentage > 5.0:
+            bot.send_message(chat_id=chat_id, text='CPU percentage is over 5%! Current value: {}'.format(cpu_percentage))
+    except Exception as e:
+        # Print any exceptions that occur
+        print(e)
+
+    # Sleep for 5 seconds before checking again
+    time.sleep(5)
