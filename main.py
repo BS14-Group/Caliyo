35fd783cedd917db41086feb062b6bd05fab6f2c
FlyingPretzel <114416661+FlyingPretzel-Labs@users.noreply.github.com>
2022-12-19 21:17:17 +0100

    Update main.py
    Updated visuals to send message on submit (for presentation)

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import telegram
 
 bot = telegram.Bot(token="5839085506:AAGyxBqxfhHG_pG9kr2PCtKBE6J4SSXkBVc")
 
+# Get the current CPU percentage
+cpu_percentage = psutil.cpu_percent()
+
+bot.sendMessage(chat_id=1560942074, text="System started (Healthy) CPU percentage: {}".format(cpu_percentage))
+
 while True:
     try:
         # Get the current CPU percentage
@@ -15,8 +20,8 @@ while True:
         # Print the CPU percentage
         print('System CPU load is {} %'.format(cpu_percentage))
 
-        # If the CPU percentage is over 1%, send a notification
-        if cpu_percentage > 0.5:
+        # If the CPU percentage is over 4%, send a notification
+        if cpu_percentage > 4.0:
             bot.sendMessage(chat_id=1560942074, text="CPU percentage is over 1%! Current value: {}".format(cpu_percentage))
             #the break can be removed if we want to continuously run the script
             break
@@ -26,4 +31,3 @@ while True:
     except Exception as e:
         # Print any exceptions that occur
         print(e)
-	
